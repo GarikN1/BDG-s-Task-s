21e308227d4c1a1404977c213bb2342b9927182e
Garik N1 <garikhakobyansamveli@gmail.com>
2023-12-15 15:52:57 +0400

    Add files via upload

---
+++ Loops_tasks.py
@@ -0,0 +1,69 @@
+#Loops tasks
+ # Print the even numbers from 0 to 20 using a for loop and the range function
+
+# for num in range(0, 21, 2):
+#     print(num)
+
+
+
+# Print the multiplication table of 5 using a for loop and f”strings”. The table should be
+# printed up to 10.
+
+
+# for i in range(1, 11):
+#     result = 5 * i
+#     print(f"5 x {i} = {result}")
+
+
+
+# # Print a square pattern using a nested for loop. The pattern should have 5 rows and 5
+# # columns.
+
+# for row in range(5):
+#     for col in range(5):
+#         print("*", end=" ")
+#     print()
+
+
+
+#Print a right-angled triangle pattern using a nested for loop. The pattern should have 5
+# rows
+
+
+# for i in range(1, 6):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+
+
+
+# ex.2 Print a square pattern using a nested for loop. The pattern should have 5 rows and 5
+# columns.
+
+# for row in range(5):
+#     for col in range(5):
+#         print("*", end=" ")
+#     print()
+
+
+
+# # ex.3 Print a right-angled triangle pattern using a nested for loop. The pattern should have 5
+# # rows
+
+
+# for i in range(1, 6):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+
+
+# Exercise 4:
+# Print the multiplication table of 5 using a for loop and f”strings”. The table should be
+# printed up to 10.
+
+
+# for i in range(1, 11):
+#     result = 5 * i
+#     print(f"5 x {i} = {result}")
