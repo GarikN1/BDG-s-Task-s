c5efa07a579fefa791099231aca3f7acfa66307b
Garik N1 <garikhakobyansamveli@gmail.com>
2023-12-15 15:44:10 +0400

    Add files via upload

---
+++ Moduls.py
@@ -0,0 +1,47 @@
+# Write a python program which will check is your number equal
+# the random number of computer (1-10)if yes print True otherwise
+# False.
+
+
+# import random
+# number = random.randint(1,10)
+# number_2 = int(input())
+# if number_2 == number:
+#     print(True)
+# else: print(False)
+
+
+
+
+
+# Write a python function which will get a number and check is
+# your number great or equal the random number of computer
+# (10-100)if yes print True otherwise False.
+
+# import random
+
+# def check_number(input_number):
+#     numbers = random.randint(10,100)
+#     return input_number >= numbers
+# number_input = int(input())
+# result = check_number(number_input)
+# print(result)
+
+
+
+
+
+
+# Write a python function which will get circle_r(շառավիղ)
+# Compute the area of a circle.
+
+# import math
+
+# def compute_circle_area(circle_r):
+#     area = math.pi * circle_r**2
+#     return area
+# radius = float(input())
+# circle_area = compute_circle_area(radius)
+# print(radius,circle_area)
+
+
